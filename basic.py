f6a2b43799f29c97cc7b4ff0842653c9572b42e8
Official21A <officialamirhossein21@gmail.com>
2020-11-13 09:52:11 +0330

    bug fixed

--- basic.py
+++ basic.py
@@ -2,14 +2,29 @@
 number1 = input("Enter A >> ")
 number2 = input("Enter B >> ")
 
-list1 = number1.split(sep=None)
-list2 = number2.split(sep=None)
+a = len(number1)
+b = len(number2)
+extra = abs(a - b)
+
+if a > b:
+	number2 = ("0" * extra) + number2
+elif b > a:
+	number1 = ("0" * extra) + number1
+
+list1 = [number1[i] for i in range(len(number1))]
+list2 = [number2[i] for i in range(len(number2))]
+
 
 flag = True
 
 for x, y in zip(list1, list2):
+
+	if x == "." or y == ".":
+		continue
+
 	x = int(x)
 	y = int(y)
+
 	if x > y:
 		print("A_GT_B")
 		flag = False
