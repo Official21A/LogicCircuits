f670ae4b85bc2c61281d574d84d2333041320e98
Official21A <officialamirhossein21@gmail.com>
2020-11-14 08:00:50 +0330

    refactor

--- binary.py
+++ binary.py
@@ -4,25 +4,22 @@
 
 def to_bin(number, places = 3): 
   
-    whole, dec = str(number).split(".") 
+    real, dec = number.split(".") 
 
-    if len(whole) == 0:
-        whole = ("0")
+    if len(real) == 0:
+        real = ("0")
     if len(dec) == 0:
         dec = ("0")
   
-    whole = int(whole) 
+    real = int(real) 
     dec = int (dec) 
   
-    res = bin(whole).lstrip("0b") + "."
+    res = bin(real).lstrip("0b") + "."
   
     for x in range(places): 
-  
-        whole, dec = str((decimal_converter(dec)) * 2).split(".") 
-  
+        real, dec = str((decimal_converter(dec)) * 2).split(".") 
         dec = int(dec) 
-  
-        res += whole 
+        res += real 
   
     return res 
   
@@ -31,15 +28,3 @@ def decimal_converter(num):
     while num > 1: 
         num /= 10
     return float(num) 
-  
-
-  
-# Take the user input for  
-# the floating point number 
-n = input("Enter your floating point value : \n") 
-  
-# Take user input for the number of 
-# decimal places user want result as 
-p = int(input("Enter the number of decimal places of the result : \n")) 
-  
-print(to_bin(n, places = p)) 
